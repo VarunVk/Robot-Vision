a6d6c6f09a6df44f6474e1cb0443758960f7399b
vijay094 <vijay094@umn.edu>
2018-04-10 13:24:40 -0500

    Update h2tr.m

--- PoseEstimationAssn/h2tr.m
+++ PoseEstimationAssn/h2tr.m
@@ -3,18 +3,17 @@ function pose = h2tr(K, H)
     
     % First Row is  R1
     R1 = ProdR1R2t(:, 1);
-    R1norm = norm(R1);
-    R1 = R1/R1norm;
+    s = norm(R1);
+    R1 = R1/s;
     
     % Second Row is  R2
-    R2 = ProdR1R2t(:, 2);
-    R2 = R2/norm(R2);
+    R2 = ProdR1R2t(:, 2)/s;
         
     % Cross product of  R1 and R2
     R1xR2 = cross(R1, R2);
     
     % Third Row is t
-    t = ProdR1R2t(:,3)/R1norm;
+    t = ProdR1R2t(:,3)/s;
     
     pose = [R1 R2 R1xR2 t; 0 0 0 1];
 end
